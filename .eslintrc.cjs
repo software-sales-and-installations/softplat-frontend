7b60624327ab6654096a4bb8b24eabeee2e4f77e
Evgeniy Lobanov <evglbnv@yandex.ru>
2023-10-30 07:14:47 +0300

    ESlint airbnb setup

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,6 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  extends: 'airbnb-base',
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'no-underscore-dangle': ['error', { allow: ['_id'] }],
   },
-}
+};
