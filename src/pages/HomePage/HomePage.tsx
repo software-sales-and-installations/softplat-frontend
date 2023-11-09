f50588a8a77a99136d074d9375bf4c7bd350060c
Kirill Kalinin <107874838+KiryhaUdmurt@users.noreply.github.com>
2023-11-09 20:58:01 +0300

    Revert "Feat/slider catalogue card"

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,14 +1,12 @@
 import { FC } from 'react';
-import Catalogue from '../../components/Catalogue/Catalogue';
-import Slider from '../../components/Slider/Slider';
-import Recommended from '../../components/Recommended/Recommended';
+import { Header } from '../../components/Header/Header';
+import { Categories } from '../../components/Categories/Categories';
 
 export const HomePage: FC = () => {
     return (
         <>
-        <Slider />
-        <Catalogue />
-        <Recommended />
+        <Header loggedIn={true}/>
+        <Categories />
         </>
     )
 }
