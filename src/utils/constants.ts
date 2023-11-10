b461d94a1d4e6b95e02d679700049415e4cced74
Kirill Kalinin <kalinin1507.2012@gmail.com>
2023-11-10 10:42:43 +0400

    fix: исправляю мерж

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -45,3 +45,14 @@ export const CATEGORIZED_TEXT: {
     {id: 10,
     text: 'Корпоративные системы'}
 ]
+
+export const CATALOGUE_NAMES:{name: string; img: string}[] = [
+  {name: 'Офисные приложения', img: ''},
+  {name: 'Системное ПО', img: ''},
+  {name: 'Мультимедиа', img: ''},
+  {name: 'Конвекторы', img: ''},
+  {name: 'Архиваторы', img: ''},
+  {name: 'Безопасность', img: ''},
+  {name: 'Интернет', img: ''},
+  {name: 'ERP & CRM', img: ''},
+];
