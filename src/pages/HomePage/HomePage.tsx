d32dc2825e7955741b2225fcd55d4e91fe95005e
WhiteWat3r <goirkopf@gmail.com>
2023-11-10 13:31:08 +0300

    resolve conflicts

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,8 +1,14 @@
 import { FC } from 'react';
+import Catalogue from '../../components/Catalogue/Catalogue';
+import Slider from '../../components/Slider/Slider';
+import Recommended from '../../components/Recommended/Recommended';
 
 export const HomePage: FC = () => {
     return (
         <>
+        <Slider />
+        <Catalogue />
+        <Recommended />
         </>
     )
 }
