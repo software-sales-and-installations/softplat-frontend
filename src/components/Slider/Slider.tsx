674b9f018f77931d691108fcb171f354f88199d6
Kirill Kalinin <kalinin1507.2012@gmail.com>
2023-11-14 20:10:00 +0400

    fear: стилизовал слайдер

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,30 +1,29 @@
-import styles from './Slider.module.scss';
+import './Slider.scss'
+
+// import 'swiper/css';
+// import 'swiper/css/pagination';
 
 import 'swiper/scss';
-import 'swiper/scss/navigation';
+import 'swiper/scss/pagination';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation } from 'swiper/modules';
+import { Pagination } from 'swiper/modules';
 
 type Slider = {
   text: string;
-  url: string;
   img: string;
 }[];
 
 const slides: Slider = [
   {
-    text: 'Slide 1',
-    url: 'https://github.com/',
+    text: 'Быстрый и удобный процесс покупки — получите доступ к программному обеспечению всего в несколько кликов',
     img: 'https://randomwordgenerator.com/img/picture-generator/sea-2101488_640.jpg',
   },
   {
-    text: 'Slide 2',
-    url: 'https://github.com/',
+    text: 'Удобные способы оплаты онлайн в любое время',
     img: '',
   },
   {
     text: 'Slide 3',
-    url: 'https://github.com/',
     img: '',
   },
 ];
@@ -32,32 +31,33 @@ const slides: Slider = [
 type Props = {};
 
 const Slider = (props: Props) => {
+
   return (
+
     <Swiper
       spaceBetween={40}
       slidesPerView={'auto'}
-      mousewheel={true}
-      modules={[Navigation]}
+      // mousewheel={true}
+      modules={[Pagination]}
       loop={true}
-      navigation
-      className={styles.swiper}
+      // navigation
+      pagination={{clickable: true}}
+      className='swiper'
     >
       {slides.map(i => (
-        <SwiperSlide className={styles['swiper-slide']}>
-          <a
-            className={styles['swiper-slide__content']}
-            href={i.url}
-            target="_blank"
+        <SwiperSlide className='swiper-slide'>
+          <div
+            className='swiper-slide__content'
           >
             <img
-              className={styles['swiper-slide__img']}
+              className='swiper-slide__img'
               src={i.img}
               alt="Картинка слайдера"
             />
-            <div className={styles['swiper-slide__text-container']}>
-              <p className={styles['swiper-slide__text']}>{i.text}</p>
+            <div className='swiper-slide__text-container'>
+              <p className='swiper-slide__text'>{i.text}</p>
             </div>
-          </a>
+          </div>
         </SwiperSlide>
       ))}
     </Swiper>
