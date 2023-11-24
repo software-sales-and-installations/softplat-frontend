3173e75c21ccafa9266746a3327e8f1950d160fc
YuliaDuk <yule444ka1234@mail.ru>
2023-11-24 20:46:04 +0300

    fix: type of cards from server

--- src/components/CardsGrid/CardsGrid.tsx
+++ src/components/CardsGrid/CardsGrid.tsx
@@ -6,7 +6,7 @@ import { CardsGridProps } from './CardsGridTypes';
 const CardsGrid: FC<CardsGridProps> = ({cards}) => {
   return (
     <ul className={styles.list}>
-      {cards?.map(i => (
+      {cards?.products.map(i => (
         <li className={styles.item} key={i.id}>
           <ProductCard
             name={i.name}
