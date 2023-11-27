e9a54a24ec095d7fc632a9a7a5c90d422fab8f37
YuliaMenachoPages <yumenachopages@gmail.com>
2023-11-27 17:31:08 +0300

    build

--- src/services/redux/store.ts
+++ src/services/redux/store.ts
@@ -23,8 +23,6 @@ export const store = configureStore({
 
 export type RootState = ReturnType<typeof store.getState>;
 
-
-
 export type AppDispatch = typeof store.dispatch;
 export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
