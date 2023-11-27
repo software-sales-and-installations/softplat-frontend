de524ba74a99caa12f6cf2fbcdec7ea247f34360
YuliaDuk <yule444ka1234@mail.ru>
2023-11-27 22:00:50 +0300

    feat: защищенные маршруты

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,13 @@ import { ProductPage } from './pages/ProductPage/ProductPage';
 import {FAQ} from './pages/FAQ/FAQ';
 import VendorPage from './pages/VendorPage/VendorPage';
 import Contacts from './pages/Contacts/contacts.tsx';
-// import Catalog from './pages/Catalog/Catalog.tsx';
 import CatalogSection from './pages/CatalogSection/CatalogSection.tsx';
 import Personal from './pages/Personal/Personal.tsx';
 import { Producers } from './pages/Producers/Producers.tsx';
 import { ShoppingCart } from './pages/ShoppingCart/ShoppingCart.tsx';
 import Search from './pages/Search/Search.tsx';
 import { Seller } from './pages/Seller/Seller.tsx';
+import { ProtectedRouteForAdmin, ProtectedRouteForBuyer, ProtectedRouteForSeller } from './components/ProtectedRoute/ProtectedRoute.tsx';
 
 function App() {
   return (
@@ -25,18 +25,54 @@ function App() {
           <Route path="/catalog" element={<HomePage />} />
           <Route path="/product/:id" element={<ProductPage />} />
           <Route path='/faq' element={<FAQ/>} />
-          {/* <Route path='/vendor' element={<VendorPage />} /> */}
           <Route path="/contacts" element={<Contacts />} />
-          {/* <Route path="/catalog" element={<Catalog />} /> */}
           <Route path="/catalog/:section" element={<CatalogSection />} />
-          <Route path="/personal/*" element={<Personal />} />
-          <Route path="/personal" element={<Navigate to='/personal/purchases' />} />
+          <Route 
+            path="/personal/*" 
+            element={
+              <ProtectedRouteForBuyer>
+                <Personal />
+              </ProtectedRouteForBuyer>
+              } />
+          <Route 
+            path="/personal" 
+            element={
+              <ProtectedRouteForBuyer>
+                <Navigate to='/personal/purchases' />
+              </ProtectedRouteForBuyer>
+            } />
           <Route path='/producers' element ={<Producers/>}/>
           <Route path='/producers/:vendor' element={<VendorPage/>}/>
           <Route path="/cart" element={<ShoppingCart />} />
           <Route path="/search" element={<Search />} />
-          <Route path="/seller/*" element={<Seller />} />
-          <Route path="/seller" element={<Navigate to="/seller/products" />} />
+          <Route 
+            path="/seller/*" 
+            element={
+              <ProtectedRouteForSeller>
+                <Seller />
+              </ProtectedRouteForSeller>
+            } />
+          <Route 
+            path="/seller" 
+            element={
+              <ProtectedRouteForSeller>
+                <Navigate to="/seller/products" />
+              </ProtectedRouteForSeller>
+            } />
+          <Route 
+            path="/admin/*" 
+            element={
+              <ProtectedRouteForAdmin>
+                <></>
+              </ProtectedRouteForAdmin>
+            } />
+          <Route 
+            path="/admin" 
+            element={
+              <ProtectedRouteForAdmin>
+                <Navigate to="/admin/product-cards" />
+              </ProtectedRouteForAdmin>
+            } />
         </Routes>
       </MainWrapper>
       <Footer />
