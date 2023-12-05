177b3785d9987b59ce2f023373e6ec3e7ac4cbab
YuliaDuk <yule444ka1234@mail.ru>
2023-12-05 12:10:36 +0300

    feat: хедер под новый дизайн

--- src/components/HeaderNavBar/HeaderNavBar.tsx
+++ src/components/HeaderNavBar/HeaderNavBar.tsx
@@ -3,14 +3,19 @@ import { Link } from 'react-router-dom';
 import styles from './HeaderNavBar.module.scss';
 import { useLocation } from 'react-router-dom';
 import classNames from 'classnames';
+import { IoIosArrowDown } from "react-icons/io";
 
 export const HeaderNavbar: FC = () => {
     const location = useLocation();
+    function handleClick(){
+        console.log('Сюда добавить дропдаун с категориями')
+    }
     return (
         <nav>
             <ul className={styles.list}>
                 <li className={styles.item}>
                     <Link to='/catalog' className={classNames(styles.link, location.pathname==='/catalog' ? styles.link_active : '')}>Каталог</Link>
+                    <button type='button' onClick={handleClick} className={styles.btn}><IoIosArrowDown /></button>
                 </li>
                 <li className={styles.item}>
                     <Link to='/producers' className={classNames(styles.link, location.pathname==='/producers' ? styles.link_active : '')}>Производители</Link>
