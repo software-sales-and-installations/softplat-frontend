f80995be022d3fcbc92bc477b678f664e9e9a296
evgeny <evgeny@158.160.8.209>
2023-12-06 12:47:58 +0300

    сделал иконки+кнопки

--- src/UIStorybook/Button/Button.stories.tsx
+++ src/UIStorybook/Button/Button.stories.tsx
@@ -1,5 +1,4 @@
 import type { Meta, StoryObj } from '@storybook/react';
-
 import { Button } from './Button.tsx';
 
 const meta: Meta<typeof Button> = {
@@ -17,45 +16,53 @@ type Story = StoryObj<typeof meta>;
 export const Primary: Story = {
   args: {
     buttonType: 'primary',
-    children: 'Добавить в корзину'
+    children: 'Добавить в корзину',
   },
 };
 
 export const Secondary: Story = {
   args: {
     buttonType: 'secondary',
-    children: 'Text'
+    children: 'Text',
   },
 };
 
 export const Decline: Story = {
   args: {
     buttonType: 'decline',
-    children: 'Отправить на доработку'
+    children: 'Отправить на доработку',
   },
 };
 
 export const Agree: Story = {
   args: {
     buttonType: 'agree',
-    children: 'Одобрить'
+    children: 'Одобрить',
   },
 };
 
-export const Minor: Story = {
+export const MinorLeft: Story = {
   args: {
-    buttonType: 'minor',
+    buttonType: 'minorLeft',
+  },
+};
+
+export const MinorRight: Story = {
+  args: {
+    buttonType: 'minorRight',
   },
 };
 
 export const Square: Story = {
   args: {
     buttonType: 'square',
+    children: 'А',
   },
 };
 
 export const Link: Story = {
   args: {
     buttonType: 'link',
+    children: 'Системное ПО',
   },
 };
