c8295b89083d6bd8daf790d18b14a61bc8a4bd1b
YuliaDuk <yule444ka1234@mail.ru>
2023-12-08 15:29:02 +0300

    fix: изменена таблица в кабинете админа

--- src/components/AdminCardTable/AdminCardTable.tsx
+++ src/components/AdminCardTable/AdminCardTable.tsx
@@ -11,90 +11,132 @@ export const AdminCardTable: FC<IProductCardPropsTable> = ({ products , productS
       <table>
         <thead>
           <tr className={classNames(styles.line, styles.line_type_head)}>
-            <th className={classNames(styles.cellName, styles.cell)}>
+            <th className={classNames(styles.cellName, styles.cell, styles.cell_type_head)}>
               Название
             </th>
-            {location.pathname !=='/admin/appeal' ? <th className={classNames(styles.cellVendorCategory, styles.cell)}>
+            {location.pathname !=='/admin/appeal' ? <th className={classNames(styles.cellVendor, styles.cell, styles.cell_type_head)}>
               Вендор
             </th> : null}
-            <th className={classNames(styles.cellSeller, styles.cell)}>
+            <th className={classNames(styles.cellSeller, styles.cell, styles.cell_type_head)}>
               Продавец
             </th>
-            <th className={classNames(styles.cellArt, styles.cell)}>Артикул</th>
-            {location.pathname ==='/admin/appeal' ?<><th className={classNames(styles.cellDescription, styles.cell)}>
+            <th className={classNames(styles.cellArt, styles.cell, styles.cell_type_head)}>Артикул</th>
+            {location.pathname ==='/admin/appeal' ?<><th className={classNames(styles.cellDescription, styles.cell, styles.cell_type_head)}>
               Жалобы
             </th>
-            <th className={classNames(styles.cellDescription, styles.cell)}>
+            <th className={classNames(styles.cellDescription, styles.cell, styles.cell_type_head)}>
               Новые
             </th></> : null}
-            {location.pathname !=='/admin/appeal' ? <th className={classNames(styles.cellData, styles.cell)}>Дата</th> : null}
+            {location.pathname !=='/admin/appeal' ? <th className={classNames(styles.cellData, styles.cell, styles.cell_type_head)}>Дата</th> : null}
           </tr>
         </thead>
         <tbody>
-          {products.map(item => {
-            return (
+       {products.map((i)=>{
+        return (
+        <tr className={styles.line} key={i.id}>
+          {i.productStatus===productStatus?
+          <>
+            <td className={classNames(styles.cellName, styles.cell)}>
+              <Link to={`/product/${i.id}`} className={styles.link}>
+                <p className={styles.cell__text}>{i.name}</p>
+              </Link>
+            </td>
+            {location.pathname !=='/admin/appeal' ? 
+            <td className={classNames(styles.cellVendor, styles.cell)}>
+              <Link to={`/product/${i.id}`} className={styles.link}>
+                <p className={classNames(styles.cell__text) }>{i.vendor?.name ? i.vendor.name : ''}</p>
+              </Link>
+            </td> : null}
+            <td className={classNames(styles.cellSeller, styles.cell)}>
+              <Link to={`/product/${i.id}`} className={styles.link}>
+                <p className={styles.cell__text}>{i.vendor?.name ? i.vendor.name : ''}</p>
+              </Link>
+            </td>
+            <td className={classNames(styles.cellArt, styles.cell)}>
+              <Link to={`/product/${i.id}`} className={styles.link}>
+                <p className={styles.cell__text}>{i.id}</p>
+              </Link>
+            </td>
+            {location.pathname ==='/admin/appeal' ?
             <>
-            {item.productStatus===productStatus ?
-              <tr
-                key={item.id}
-                className={classNames(styles.line, styles.line_type_body)}
-              >
-                  <td className={classNames(styles.cellLogo, styles.cell)}>
-                    <Link to={`/product/${item.id}`} className={styles.link}>
-                      <img
-                        className={classNames(
-                          styles.cellLogo__img,
-                          styles.cell,
-                        )}
-                        src={item.seller?.imageResponseDto?.url}
-                        alt={item.name}
-                      />
-                    </Link>
-                  </td>
-                <td className={classNames(styles.cellName, styles.cell)}>
-                  <Link to={`/product/${item.id}`} className={styles.link}>
-                    <p className={styles.cell__text}>{item.name}</p>
-                  </Link>
-                </td>
-                <td
-                  className={classNames(styles.cellVendorCategory, styles.cell)}
-                >
-                  <Link to={`/product/${item.id}`} className={styles.link}>
-                    <p className={styles.cell__text}>
-                      {item.vendor?.name ? item.vendor.name : ''}
-                    </p>
-                  </Link>
-                </td>
-                <td
-                  className={classNames(styles.cellVendorCategory, styles.cell)}
-                >
-                  <Link to={`/product/${item.id}`} className={styles.link}>
-                    <p className={styles.cell__text}>
-                      {item.category?.name ? item.category.name : ''}
-                    </p>
-                  </Link>
-                </td>
-                <td className={classNames(styles.cellArt, styles.cell)}>
-                  <Link to={`/product/${item.id}`} className={styles.link}>
-                    <p className={styles.cell__text}>{item.id}</p>
-                  </Link>
-                </td>
-                <td className={classNames(styles.cellDescription, styles.cell)}>
-                  <Link to={`/product/${item.id}`} className={styles.link}>
-                    <p className={styles.cell__text}>{item.description}</p>
-                  </Link>
-                </td>
-                {/* <td className={classNames(styles.cellData, styles.cell)}>
-                  <Link to={`/product/${item.id}`} className={styles.link}>
-                    <p className={styles.cell__text}>{item.date}</p>
-                  </Link>
-                </td> */}
+            <td>
+              <p>кол-во жалоб</p>
+            </td>
+            <td>
+              <p>новые жалобы</p>
+            </td>
+            </>: null}
+            {location.pathname !=='/admin/appeal' ?<td className={classNames(styles.cellArt, styles.cell)}>
+              <Link to={`/product/${i.id}`} className={styles.link}>
+                <p className={styles.cell__text}>{`${i.productionTime? i.productionTime: ''}`}</p>
+              </Link>
+            </td>: null}
+            <td className={classNames(styles.cellBtn, styles.cell)}>
+              <button className={styles.cell__btn}></button>
+            </td>
+            </>
+          :null}
 
-              </tr>
-              
-             : null};
-          </>)})}
+        </tr>)
+       })}
+
+
+
         </tbody>
       </table>
-  );
-};
+    );
+    };
+            
+            // (item.productStatus===productStatus ?
+            //   <tr
+            //   key={item.id}
+            //     className={classNames(styles.line, styles.line_type_body)}
+            //   >
+            //       <td className={classNames(styles.cellLogo, styles.cell)}>
+            //         <Link to={`/product/${item.id}`} className={styles.link}>
+            //           <img
+            //             className={classNames(
+            //               styles.cellLogo__img,
+            //               styles.cell,
+            //             )}
+            //             src={item.seller?.imageResponseDto?.url}
+            //             alt={item.name}
+            //           />
+            //         </Link>
+            //       </td>
+            //     <td className={classNames(styles.cellName, styles.cell)}>
+            //       <Link to={`/product/${item.id}`} className={styles.link}>
+            //         <p className={styles.cell__text}>{item.name}</p>
+            //       </Link>
+            //     </td>
+            //     <td
+            //       className={classNames(styles.cellVendorCategory, styles.cell)}
+            //     >
+            //       <Link to={`/product/${item.id}`} className={styles.link}>
+            //         <p className={styles.cell__text}>
+            //           {item.vendor?.name ? item.vendor.name : ''}
+            //         </p>
+            //       </Link>
+            //     </td>
+            //     <td
+            //       className={classNames(styles.cellVendorCategory, styles.cell)}
+            //     >
+            //       <Link to={`/product/${item.id}`} className={styles.link}>
+            //         <p className={styles.cell__text}>
+            //           {item.category?.name ? item.category.name : ''}
+            //         </p>
+            //       </Link>
+            //     </td>
+                // <td className={classNames(styles.cellArt, styles.cell)}>
+                //   <Link to={`/product/${item.id}`} className={styles.link}>
+                //     <p className={styles.cell__text}>{item.id}</p>
+                //   </Link>
+                // </td>
+            //     <td className={classNames(styles.cellDescription, styles.cell)}>
+            //       <Link to={`/product/${item.id}`} className={styles.link}>
+            //         <p className={styles.cell__text}>{item.description}</p>
+            //       </Link>
+            //     </td>
+            //   </tr>)
+            //  : null}))}
+
