dea7741d5690765b4023c4e68388d285ebeb4bde
YuliaDuk <yule444ka1234@mail.ru>
2023-12-15 12:34:41 +0300

    feat: страница вендоры для админа

---
+++ src/components/AdminVendorsCatalog/AdminVendorsCatalog.tsx
@@ -0,0 +1,32 @@
+import {FC, useEffect, useState} from 'react';
+import styles from './AdminVendorsCatalog.module.scss';
+import { useVendorListQuery } from '../../utils/api/vendorApi';
+import { Link } from 'react-router-dom';
+
+export const AdminVendorsCatalog: FC = () =>{
+    const { data: vendorAll,
+        //     // isFetching,isLoading, error
+          } = useVendorListQuery();
+    const [vendorData, setVendorData] = useState(vendorAll)
+          useEffect(()=>{
+            setVendorData(vendorAll)
+          },[vendorData, vendorAll])
+    return (
+        <div className={styles.table}>
+            <div className={styles.table__head}>
+                <p className={styles.table__vendor}>Вендор</p>
+                <p className={styles.table__logo}>Логотип</p>
+                <p className={styles.table__country}>Страна</p>
+            </div>
+            {vendorData?.vendors.map((i)=>{
+                return(
+                    <Link to={`/producers/${i.id}`} className={styles.table__line}>
+                        <p className={styles.table__vendor}>{i.name}</p>
+                        <p className={styles.table__logo}>{i.image?.url? i.image?.url : ''}</p>
+                        <p className={styles.table__country}>{i.country}</p>
+                    </Link>
+                )
+            })}
+        </div>
+    )
+}
