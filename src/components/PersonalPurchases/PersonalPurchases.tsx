21ad677ccd45c9abdb72a61439c7edd7d5be8ee0
sm1ke <smike143@yandex.ru>
2023-12-16 17:21:03 +0500

    feat: реализована передача купленых товаров с сервера

--- src/components/PersonalPurchases/PersonalPurchases.tsx
+++ src/components/PersonalPurchases/PersonalPurchases.tsx
@@ -1,14 +1,43 @@
+import React from 'react';
 import styles from './PersonalPurchases.module.scss';
 import CardPurchases from '../CardPurchases/CardPurchases';
-import { PURCHASES_ITEMS_CABINET } from '../../utils/constants';
+import { useOrderAllQuery } from '../../utils/api/buyerOrderApi';
+import { cardPurchasesProps } from '../CardPurchases/CardPurchases';
+
+type Product = Omit<cardPurchasesProps, 'data'>;
 
 const PersonalPurchases: React.FC = () => {
+  const { data: purchaseItems } = useOrderAllQuery(
+    localStorage.getItem('userId'),
+    { refetchOnMountOrArgChange: true },
+  );
+
   return (
     <section className={styles.personalPurchases}>
       <ul className={styles.personalPurchases__list}>
-        {PURCHASES_ITEMS_CABINET.map(i => (
-          <CardPurchases key={i.id} {...i} />
-        ))}
+        {purchaseItems &&
+          purchaseItems.orders.map(
+            (item: {
+              id: number;
+              productsOrdered: {
+                id: number;
+                productResponseDto: Product;
+              }[];
+              productionTime: string;
+            }) => (
+              <li key={item.id}>
+                {item.productsOrdered.map(
+                  (product: { id: number; productResponseDto: Product }) => (
+                    <CardPurchases
+                      key={product.id}
+                      data={item.productionTime}
+                      {...product.productResponseDto}
+                    />
+                  ),
+                )}
+              </li>
+            ),
+          )}
       </ul>
     </section>
   );
