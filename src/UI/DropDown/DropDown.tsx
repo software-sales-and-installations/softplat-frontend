5cecf5e4dfef09dce1a43a3266f0c2c141f886ad
Kirill Kalinin <kalinin1507.2012@gmail.com>
2023-12-17 17:42:21 +0400

    поиск в дд

--- src/UI/DropDown/DropDown.tsx
+++ src/UI/DropDown/DropDown.tsx
@@ -48,9 +48,10 @@ const DropDown: FC<IDropDowmProps> = ({ options, type, isMultiOption }) => {
       closeMenuOnSelect={catalogSelect && true}
       defaultValue={baseSelect && currentBase}
       onChange={e => handleChange(e)}
-      isSearchable={true}
+      isSearchable={isMultiOption && true}
       placeholder={true}
-      menuIsOpen
+      noOptionsMessage={() => 'Нет подходящих вариантов'}
+      // menuIsOpen={!catalogSelect && true}
     />
   );
 };
