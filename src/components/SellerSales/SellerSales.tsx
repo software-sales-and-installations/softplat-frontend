6f9702e1aea62d4a27cf3336e1b7dd3c805f2ab8
sm1ke <smike143@yandex.ru>
2023-12-18 22:51:28 +0500

    feat: создать страницу Отчеты продаж

---
+++ src/components/SellerSales/SellerSales.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './SellerSales.module.scss';
+
+const SellerSales: React.FC = () => {
+  return <section className={styles.sales}></section>;
+};
+
+export default SellerSales;
