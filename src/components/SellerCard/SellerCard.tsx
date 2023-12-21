67770618aef0added572e89aee9ad1b6cc03b6ae
sm1ke <smike143@yandex.ru>
2023-12-21 20:43:10 +0500

    feat: модифицировать компонент карточек для работы на разных страницах лк

--- src/components/SellerCard/SellerCard.tsx
+++ src/components/SellerCard/SellerCard.tsx
@@ -1,23 +1,16 @@
 import React from 'react';
 import styles from './SellerCard.module.scss';
 import trashIcon from '../../images/trash.svg';
+import { ISellerCardProps } from './SellerCardTypes';
 
-type SellerCardProps = {
-  image: string;
-  name: string;
-  vendor: {
-    name: string;
-  };
-  productionTime: string;
-  trash: boolean;
-};
-
-const SellerCard: React.FC<SellerCardProps> = ({
+const SellerCard: React.FC<ISellerCardProps> = ({
+  mode,
   image,
   name,
   vendor,
   productionTime,
   trash,
+  count,
 }) => {
   const [isHovered, setIsHovered] = React.useState(false);
 
@@ -27,14 +20,28 @@ const SellerCard: React.FC<SellerCardProps> = ({
       onMouseOver={() => setIsHovered(true)}
       onMouseOut={() => setIsHovered(false)}
     >
-      <div className={styles.card__imgContainer}>
-        <img src={image} alt="Логотип продукта" className={styles.card__img} />
+      <div
+        className={`${styles.card__container} ${
+          mode === 'inOrder' ? styles.card__container_mode_inOrder : ''
+        }`}
+      >
+        {mode === 'inOrder' ? (
+          count
+        ) : (
+          <img
+            src={image}
+            alt="Логотип продукта"
+            className={styles.card__img}
+          />
+        )}
       </div>
-      <h2 className={styles.card__name}>{name}</h2>
-      <p className={styles.card__vendor}>{vendor.name}</p>
+      <h2 className={styles.card__name}>{name ? name : '—'}</h2>
+      <p className={styles.card__vendor}>{vendor.name ? vendor.name : '—'}</p>
       <p className={styles.card__number}>123456789</p>
       <p className={styles.card__date}>
-        {productionTime.split(' ')[0].split('-').join('.')}
+        {productionTime
+          ? productionTime.split(' ')[0].split('-').join('.')
+          : '—'}
       </p>
       {trash && (
         <img
@@ -42,7 +49,7 @@ const SellerCard: React.FC<SellerCardProps> = ({
           alt="Корзина"
           className={`${styles.card__trash} ${
             isHovered ? styles.card__trash_shown : ''
-          }`}
+          } ${mode === 'inOrder' ? styles.card__trash_mode_inOrder : ''}`}
         />
       )}
     </li>
