7050f4deee447009d5d2fff0a84ea30234488424
sm1ke <smike143@yandex.ru>
2023-12-21 20:40:08 +0500

    feat: добавить запросы на получение карточек при заходе в лк продавца

--- src/pages/Seller/Seller.tsx
+++ src/pages/Seller/Seller.tsx
@@ -12,8 +12,50 @@ import SellerPublished from '../../components/SellerPublished/SellerPublished';
 import SellerModeration from '../../components/SellerModeration/SellerModeration';
 import SellerCorrection from '../../components/SellerCorrection/SellerCorrection';
 import SellerSales from '../../components/SellerSales/SellerSales';
+import { useSellerProductListQuery } from '../../utils/api/sellerApi';
 
 export const Seller: FC = () => {
+  const { data: draftList, isSuccess: isDraftListSuccess } =
+    useSellerProductListQuery(
+      {
+        status: 'DRAFT',
+      },
+      { refetchOnMountOrArgChange: true },
+    );
+  const { data: publishedList, isSuccess: isPublishedListSuccess } =
+    useSellerProductListQuery(
+      {
+        status: 'PUBLISHED',
+      },
+      { refetchOnMountOrArgChange: true },
+    );
+  const { data: rejectedList, isSuccess: isRejectedListSuccess } =
+    useSellerProductListQuery(
+      {
+        status: 'REJECTED',
+      },
+      { refetchOnMountOrArgChange: true },
+    );
+  const { data: shippedList, isSuccess: isShippedListSuccess } =
+    useSellerProductListQuery(
+      {
+        status: 'SHIPPED',
+      },
+      { refetchOnMountOrArgChange: true },
+    );
+
+  if (
+    isDraftListSuccess &&
+    isPublishedListSuccess &&
+    isRejectedListSuccess &&
+    isShippedListSuccess
+  ) {
+    localStorage.setItem('sellerDraftList', JSON.stringify(draftList));
+    localStorage.setItem('sellerPublishedList', JSON.stringify(publishedList));
+    localStorage.setItem('sellerRejectedList', JSON.stringify(rejectedList));
+    localStorage.setItem('sellerShippedList', JSON.stringify(shippedList));
+  }
+
   return (
     <>
       <div className={styles.breadcrumbs}>
