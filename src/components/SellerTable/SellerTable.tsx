1461fa1ebb23d42b86a5469778f8a0685956679f
sm1ke <smike143@yandex.ru>
2023-12-21 20:39:23 +0500

    fix: исправить компонент таблицы

--- src/components/SellerTable/SellerTable.tsx
+++ src/components/SellerTable/SellerTable.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import styles from './SellerTable.module.scss';
+import { ISellerTableProps } from './SellerTableTypes';
 
-const SellerTable: React.FC = () => {
+const SellerTable: React.FC<ISellerTableProps> = ({ mode }) => {
   return (
     <ul className={styles.tableHeader}>
-      <li className={styles.tableHeader__item}>Лого</li>
+      <li
+        className={`${styles.tableHeader__item} ${
+          mode === 'inOrder' ? styles.tableHeader__item_mode_inOrder : ''
+        }`}
+      >
+        {mode === 'inOrder' ? '№' : 'Лого'}
+      </li>
       <li className={styles.tableHeader__item}>Наименование</li>
       <li className={styles.tableHeader__item}>Вендор</li>
       <li className={styles.tableHeader__item}>Артикул</li>
