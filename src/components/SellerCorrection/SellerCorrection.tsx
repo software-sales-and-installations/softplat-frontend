998626d5c262a16516c2a7caa376c68ac60fc4e2
sm1ke <smike143@yandex.ru>
2023-12-21 20:43:53 +0500

    feat: добавить полученные карточки на нужные страницы в лк продавца

--- src/components/SellerCorrection/SellerCorrection.tsx
+++ src/components/SellerCorrection/SellerCorrection.tsx
@@ -1,13 +1,44 @@
 import React from 'react';
 import styles from './SellerCorrection.module.scss';
 import EmptyState from '../EmptyState/EmptyState';
+import SellerCard from '../SellerCard/SellerCard';
+import SellerTable from '../SellerTable/SellerTable';
 
 const SellerCorrection: React.FC = () => {
+  const rejectedList = JSON.parse(
+    localStorage.getItem('sellerRejectedList')!,
+  ).products;
+  console.log(rejectedList);
+
   return (
     <section className={styles.correction}>
-      <EmptyState navigateTo="/seller/add-card" buttonText="Добавить карточку">
-        Карточек для доработки пока нет
-      </EmptyState>
+      {rejectedList.length === 0 ? (
+        <EmptyState
+          navigateTo="/seller/add-card"
+          buttonText="Добавить карточку"
+        >
+          Карточек для доработки пока нет
+        </EmptyState>
+      ) : (
+        <>
+          <SellerTable />
+          <ul className={styles.correction__list}>
+            {rejectedList.map(
+              (product: {
+                id: number;
+                image: string;
+                name: string;
+                vendor: {
+                  name: string;
+                };
+                productionTime: string;
+              }) => (
+                <SellerCard key={product.id} {...product} trash={true} />
+              ),
+            )}
+          </ul>
+        </>
+      )}
     </section>
   );
 };
