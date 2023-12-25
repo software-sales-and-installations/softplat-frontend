aeb516d26b922316ef34f5107f7eea75830b8386
YuliaDuk <yule444ka1234@mail.ru>
2023-12-25 08:57:24 +0300

    feat: верстка таблицы отчета

--- src/components/SellerSales/SellerSales.tsx
+++ src/components/SellerSales/SellerSales.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import styles from './SellerSales.module.scss';
 import EmptyState from '../EmptyState/EmptyState';
+import { StatsTable } from '../StatsTable/StatsTable';
 
 const SellerSales: React.FC = () => {
   return (
     <section className={styles.sales}>
       <EmptyState button={false}>Продаж пока нет</EmptyState>
+      <StatsTable/>
     </section>
   );
 };
--- src/components/StatsTable/StatsTable.tsx
+++ src/components/StatsTable/StatsTable.tsx
@@ -1,19 +1,38 @@
 import {FC} from 'react';
 import styles from './StatsTable.module.scss';
 import classNames from 'classnames';
+import { Button } from '../../UI/Button/Button';
 
 export const StatsTable: FC = () =>{
     return (
-        <>
+        <section>
         <div className={styles.statsTable__header}>
             <p className={classNames(styles.statsTable__cell, styles.statsTable__cell_type_number)}>№</p>
             <p className={classNames(styles.statsTable__cell, styles.statsTable__cell_type_name)}>Название</p>
             <p className={classNames(styles.statsTable__cell, styles.statsTable__cell_type_art)}>Артикул</p>
-            <p className={classNames(styles.statsTable__cell, styles.statsTable__cell_type_demo)}>Демо (шт)</p>
-            <p className={classNames(styles.statsTable__cell, styles.statsTable__cell_type_qty)}>Купили (шт)</p>
-            <p className={classNames(styles.statsTable__cell, styles.statsTable__cell_type_sum)}>Сумма (руб)</p>
-            <p className={classNames(styles.statsTable__cell, styles.statsTable__cell_type_proceeds)}>Выручка (руб)</p>
+            <p className={classNames(styles.statsTable__cell, styles.statsTable__cell_type_demo)}>Демо 
+                <span className={styles.statsTable__cell_type_span}> (шт)</span>
+            </p>
+            <p className={classNames(styles.statsTable__cell, styles.statsTable__cell_type_qty)}>Купили 
+                <span className={styles.statsTable__cell_type_span}> (шт)</span>
+            </p>
+            <p className={classNames(styles.statsTable__cell, styles.statsTable__cell_type_sum)}>Сумма 
+                <span className={styles.statsTable__cell_type_span}> (руб)</span>
+            </p>
+            <p className={classNames(styles.statsTable__cell, styles.statsTable__cell_type_proceeds)}>Выручка 
+                <span className={styles.statsTable__cell_type_span}> (руб)</span>
+            </p>
         </div>
-        </>
+        <div className={styles.statsTable__sum}>
+            <h3 className={styles.statsTable__sumResult}>Итого</h3>
+            <div className={styles.statsTable__sumContainer}>
+                <p className={styles.statsTable__sumResult}>22222</p>
+                <p className={styles.statsTable__sumResult}>22222</p>
+            </div>
+        </div>
+        <div className={styles.statsTable__containerforBtn}>
+            <Button type='submit' mode='primary'>Выгрузить</Button>
+        </div>
+        </section>
     )
 }
