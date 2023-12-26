ab64723908fa615f073cfef348628ed47373f214
Yulia Menacho-Pages <yulia.menacho-pages@yandex.ru>
2023-12-26 04:27:52 +0300

    calendar

--- src/components/SellerSales/SellerSales.tsx
+++ src/components/SellerSales/SellerSales.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import styles from './SellerSales.module.scss';
 import EmptyState from '../EmptyState/EmptyState';
+import Calendar from '../Calendar/Calendar.tsx';
 
 const SellerSales: React.FC = () => {
   return (
     <section className={styles.sales}>
       <EmptyState button={false}>Продаж пока нет</EmptyState>
+      <Calendar />
     </section>
   );
 };
